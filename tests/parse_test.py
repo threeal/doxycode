b9a0bd77d4315f2c7f685fbcec0c878367fb0aa8
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-20 21:07:46 +0700

    add docstrings in source codes inside the `tests` directory

--- tests/parse_test.py
+++ tests/parse_test.py
@@ -1,13 +1,17 @@
+'''Test functions for parsing comments from a source code.'''
+
 import pathlib
 import unittest
 from doxycode import parse_comments
 
 class TestParse(unittest.TestCase):
+    '''The unit testing class.'''
 
     def setUp(self):
         self.root = pathlib.Path(__file__).parent.resolve()
 
     def test_parse_comments(self):
+        '''Test a function for parsing comments from a file.'''
         with open(self.root / "sample/include/sample/sample.hpp", 'r') as file:
             comments = parse_comments(file)
             expected = ' namespace sample\n'

af20e94d1cbfdbed250afb31fee0726631d026f2
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-20 15:59:03 +0700

    add a `get_next` function to get the next char of a string at a specific index

--- doxycode/parse.py
+++ doxycode/parse.py
@@ -1,10 +1,14 @@
+def get_next(line: str, index: int) -> str:
+  return line[index + 1] if index + 1 < len(line) else ''
+
+
 def parse_comments(file):
   for line in file.readlines():
     # find the first occurrence of a forward slash
     index = line.find('/')
-    if index < 0 or index >= len(line) - 1:
+    if index < 0:
       continue
 
     # the forward slash is a part of a single-line comment
-    if line[index + 1] == '/':
+    if get_next(line, index) == '/':
       print('%s' % line[index+2:], end='')

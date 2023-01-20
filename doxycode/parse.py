022e7e04f71d3244460bd347bb8552fac6f4c710
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-20 15:53:18 +0700

    filter single-line comments in the `parse_comments` function

--- doxycode/parse.py
+++ doxycode/parse.py
@@ -1,3 +1,10 @@
 def parse_comments(file):
   for line in file.readlines():
-    print(line, end='')
+    # find the first occurrence of a forward slash
+    index = line.find('/')
+    if index < 0 or index >= len(line) - 1:
+      continue
+
+    # the forward slash is a part of a single-line comment
+    if line[index + 1] == '/':
+      print('%s' % line[index+2:], end='')

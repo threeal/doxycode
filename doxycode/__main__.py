d14cdfa609d851f8ca66bf112e2695e51465d59f
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-20 20:42:11 +0700

    change `parse_comments` function to output a string

--- doxycode/__main__.py
+++ doxycode/__main__.py
@@ -13,4 +13,5 @@ if len(sys.argv) < 2:
   exit(1)
 
 file = open(sys.argv[1], 'r')
-parse_comments(file)
+comments = parse_comments(file)
+print(comments, end='')
--- doxycode/parse.py
+++ doxycode/parse.py
@@ -2,7 +2,8 @@ def get_next(line: str, index: int) -> str:
   return line[index + 1] if index + 1 < len(line) else ''
 
 
-def parse_comments(file):
+def parse_comments(file) -> str:
+  out = ''
   for line in file.readlines():
     # find the first occurrence of a forward slash
     index = line.find('/')
@@ -11,4 +12,6 @@ def parse_comments(file):
 
     # the forward slash is a part of a single-line comment
     if get_next(line, index) == '/':
-      print('%s' % line[index+2:], end='')
+      out += line[index+2:]
+
+  return out

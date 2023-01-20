15962235c55431d0f05622b8923de742eec1618e
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-20 20:53:11 +0700

    fix source code indentation

--- doxycode/__main__.py
+++ doxycode/__main__.py
@@ -6,11 +6,11 @@ import sys
 from .parse import parse_comments
 
 if len(sys.argv) < 2:
-  executable = 'doxycode.py'
-  if len(sys.argv) > 0:
-    executable = os.path.basename(sys.argv[0])
-  print('Usage: %s <filename>' % executable)
-  exit(1)
+    executable = 'doxycode.py'
+    if len(sys.argv) > 0:
+        executable = os.path.basename(sys.argv[0])
+    print('Usage: %s <filename>' % executable)
+    exit(1)
 
 file = open(sys.argv[1], 'r')
 comments = parse_comments(file)
--- doxycode/parse.py
+++ doxycode/parse.py
@@ -1,17 +1,17 @@
 def get_next(line: str, index: int) -> str:
-  return line[index + 1] if index + 1 < len(line) else ''
+    return line[index + 1] if index + 1 < len(line) else ''
 
 
 def parse_comments(file) -> str:
-  out = ''
-  for line in file.readlines():
-    # find the first occurrence of a forward slash
-    index = line.find('/')
-    if index < 0:
-      continue
+    out = ''
+    for line in file.readlines():
+        # find the first occurrence of a forward slash
+        index = line.find('/')
+        if index < 0:
+            continue
 
-    # the forward slash is a part of a single-line comment
-    if get_next(line, index) == '/':
-      out += line[index+2:]
+        # the forward slash is a part of a single-line comment
+        if get_next(line, index) == '/':
+            out += line[index+2:]
 
-  return out
+    return out

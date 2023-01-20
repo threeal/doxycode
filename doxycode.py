6d46b2ecfb8ebff2c83599cb399bc8762aac82db
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-20 15:17:58 +0700

    only show basename of the script path

--- doxycode.py
+++ doxycode.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 
 if len(sys.argv) < 2:
-  print('Usage: %s <filename>' % sys.argv[0])
+  executable = 'doxycode.py'
+  if len(sys.argv) > 0:
+    executable = os.path.basename(sys.argv[0])
+  print('Usage: %s <filename>' % executable)
   exit(1)
 
 file = open(sys.argv[1], 'r')

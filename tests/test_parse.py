087f967417b9237286a45f42a218b3432c5521ce
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-21 12:16:29 +0700

    add `expected` directory for testing

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -9,12 +9,18 @@ class TestParse(unittest.TestCase):
 
     def setUp(self):
         self.root = pathlib.Path(__file__).parent.resolve()
+        self.source_dir = self.root / 'sample/include'
+        self.expected_dir = self.root / 'sample/expected'
 
     def test_parse_comments(self):
         '''Test a function for parsing comments from a file.'''
-        with open(self.root / "sample/include/sample/sample.hpp", 'r', encoding="utf-8") as file:
+        with open(self.source_dir / 'sample/sample.hpp', 'r', encoding='utf-8') as file:
             comments = parse_comments(file)
-            expected = ' namespace sample\n'
+            expected = ''
+            expected_file = self.expected_dir / 'sample/sample_comments.txt'
+            with open(expected_file, 'r', encoding='utf-8') as file:
+                for line in file.readlines():
+                    expected += line
             self.assertEqual(comments, expected)
 
 if __name__ == '__main__':

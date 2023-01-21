e1c4a3b3e811d0445103c1a58644bb05a3b237d4
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-22 06:14:10 +0700

    expand functions and documentations in the sample project

--- tests/sample/include/sample/sample.hpp
+++ tests/sample/include/sample/sample.hpp
@@ -1,9 +1,36 @@
 namespace sample {
 
+/** Check if a number is even
+ * @param val The number
+ * @return True if the number is even
+ */
+bool is_even(int val) { return val % 2 == 0; }
+
 /** Check if a number is odd
- * @param val the number
- * @return true if the number is odd
+ * @param val The number
+ * @return True if the number is odd
  */
 bool is_odd(int val) { return val % 2 == 1; }
+
+/** Perform check on a number */
+class Check {
+ private:
+  int val;  /**< The number */
+ public:
+  /** Construct a new number check object
+   * @param val The number
+   */
+  Check(int val) : val(val) {}
+
+  /** Check if the number is even
+   * @return True if the number is even
+   */
+  bool is_even() const { return val % 2 == 0; }
+
+  /** Check if the number is odd
+   * @return True if the number is odd
+   */
+  bool is_odd() const { return val % 2 == 1; }
+};
 
 }  // namespace sample
--- tests/sample/tests/sample_test.cpp
+++ tests/sample/tests/sample_test.cpp
@@ -2,5 +2,8 @@
 
 int main() {
   if (sample::is_odd(5) != true) return 1;
+  if (sample::is_even(6) != true) return 1;
+  if (sample::Check(5).is_odd() != true) return 1;
+  if (sample::Check(6).is_even() != true) return 1;
   return 0;
 }

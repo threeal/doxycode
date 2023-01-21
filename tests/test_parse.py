7ed9c68114eb7f9bdbb52ab59182f70d68206bc6
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-21 20:35:06 +0700

    change expected files from using text to using JSON array

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,5 +1,6 @@
 '''Test functions for parsing comments from a source code.'''
 
+import json
 import pathlib
 import unittest
 from doxycode import parse_comments
@@ -16,12 +17,11 @@ class TestParse(unittest.TestCase):
         '''Test a function for parsing comments from a file.'''
         with open(self.source_dir / 'sample/sample.hpp', 'r', encoding='utf-8') as file:
             comments = parse_comments(file)
-            expected = ''
-            expected_file = self.expected_dir / 'sample/sample_comments.txt'
+            expected_file = self.expected_dir / 'sample/sample_comments.json'
             with open(expected_file, 'r', encoding='utf-8') as file:
-                for line in file.readlines():
-                    expected += line
-            self.assertEqual(comments, expected)
+                file_txt = file.read()
+                expectations = json.loads(file_txt)
+                self.assertEqual(comments, expectations)
 
 if __name__ == '__main__':
     unittest.main()

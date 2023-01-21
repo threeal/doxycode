ee171646f2826d55390ca416a107ceb8e8b2a93a
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-21 20:00:35 +0700

    add support to add multi-line comments in a single line

--- doxycode/parse.py
+++ doxycode/parse.py
@@ -1,8 +1,13 @@
 '''Provide functions for parsing comments from a source code.'''
 
-def get_next(line: str, index: int) -> str:
+def get_next_char(line: str, index: int) -> str:
     '''Get the next character of an index from a string.'''
     return line[index + 1] if index + 1 < len(line) else ''
+
+
+def get_prev_char(line: str, index: int) -> str:
+    '''Get the previous character of an index from a string.'''
+    return line[index - 1] if index > 0 else ''
 
 
 def parse_comments(file) -> str:
@@ -10,12 +15,18 @@ def parse_comments(file) -> str:
     out = ''
     for line in file.readlines():
         # find the first occurrence of a forward slash
-        index = line.find('/')
-        if index < 0:
+        first = line.find('/')
+        if first < 0:
             continue
 
         # the forward slash is a part of a single-line comment
-        if get_next(line, index) == '/':
-            out += line[index+2:]
+        char = get_next_char(line, first)
+        if char == '/':
+            out += line[first+2:]
+        elif char == '*':
+            line = line[first+2:]
+            second = line.find('/')
+            if second >= 0 and get_prev_char(line, second) == '*':
+                out += line[:second-1]
 
     return out
--- tests/sample/include/sample/sample.hpp
+++ tests/sample/include/sample/sample.hpp
@@ -1,5 +1,6 @@
 namespace sample {
 
+/** Check if a number is odd */
 bool is_odd(int val) { return val % 2 == 1; }
 
 }  // namespace sample

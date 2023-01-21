b001075da5cf40fb92611a6af53901eefec47b9d
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-22 06:29:45 +0700

    add Doxygen code comment in the sample project

--- tests/sample/include/sample/sample.hpp
+++ tests/sample/include/sample/sample.hpp
@@ -3,12 +3,18 @@ namespace sample {
 /** Check if a number is even
  * @param val The number
  * @return True if the number is even
+ * @code
+ * assert(is_even(6));
+ * @endcode
  */
 bool is_even(int val) { return val % 2 == 0; }
 
 /** Check if a number is odd
  * @param val The number
  * @return True if the number is odd
+ * @code
+ * assert(is_odd(5));
+ * @endcode
  */
 bool is_odd(int val) { return val % 2 == 1; }
 
@@ -24,11 +30,19 @@ class Check {
 
   /** Check if the number is even
    * @return True if the number is even
+   * @code{.cpp}
+   * Check check(6);
+   * assert(check.is_even());
+   * @endcode
    */
   bool is_even() const { return val % 2 == 0; }
 
   /** Check if the number is odd
    * @return True if the number is odd
+   * @code{.cpp}
+   * Check check(5);
+   * assert(check.is_odd());
+   * @endcode
    */
   bool is_odd() const { return val % 2 == 1; }
 };

30e1c1644ba53e5920a7b28736303d0c6086692f
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-22 07:20:40 +0700

    change run module to parse Doxygen comments from a file

--- doxycode/__main__.py
+++ doxycode/__main__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-'''Parse multi line comments from a file.'''
+'''Parse Doxygen codes from a file.'''
 
 import os
 import sys
 
-from .parse import parse_multiline_comments
+from .parse import parse_multiline_comments, parse_doxygen_codes
 
 if len(sys.argv) < 2:
     EXE = 'doxycode.py'
@@ -16,6 +16,9 @@ if len(sys.argv) < 2:
 
 with open(sys.argv[1], 'r', encoding="utf-8") as file:
     comments = parse_multiline_comments(file)
-    for comment in comments:
-        for line in comment:
+    codes = parse_doxygen_codes(comments)
+    for i, code in enumerate(codes):
+        if i > 0:
+            print()
+        for line in code:
             print(line)
